fddde041ee4c1c0744ac17537aee9b0380bbe733
hmbashar <hmbashar@gmail.com>
2023-03-06 16:32:11 +0600

    weather and city dynamic

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+
+
+const loadTemperature = city => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => displayTemp(data));
+}
+
+loadTemperature('Patuakhali');
+
+const displayTemp = data => {
+    const cityName = document.getElementById('cityName');
+    const temp = document.getElementById('temperature');
+    temp.innerText = data.main.temp;
+
+    cityName.innerText = data.name;
+
+}
+
